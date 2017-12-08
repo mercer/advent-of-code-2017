2a714a27cbcbaa844648b8cdd2cbea8746efaa32
Emilian Losneanu <mercer.traieste@gmail.com>
2017-12-08 22:20:08 +0200

    add atrium support

--- src/test/kotlin/demo/tests.kt
+++ src/test/kotlin/demo/tests.kt
@@ -1,10 +1,13 @@
-package demo 
+package demo
 
-import kotlin.test.assertEquals
+import ch.tutteli.atrium.api.cc.en_UK.isGreaterThan
+import ch.tutteli.atrium.api.cc.en_UK.isLessThan
+import ch.tutteli.atrium.verbs.assert.assert
 import org.junit.Test
 
 class TestSource {
-    @Test fun f() {
-        assertEquals("Hello, world!", getGreeting())
+    @Test
+    fun f() {
+        assert(4 + 6).isLessThan(5).isGreaterThan(10)
     }
 }
