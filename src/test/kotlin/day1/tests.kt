5d401961d4a34fa6bd2f579761ba45926274a789
Emilian Losneanu <mercer.traieste@gmail.com>
2017-12-08 23:45:05 +0200

    day 1 second puzzle

--- src/test/kotlin/day1/tests.kt
+++ src/test/kotlin/day1/tests.kt
@@ -13,4 +13,13 @@ class TestSource {
         expect(day1.first("1234")).toBe(0)
         expect(day1.first("91212129")).toBe(9)
     }
+
+    @Test
+    fun shouldSolveSecondPuzzle() {
+        expect(day1.second("1212")).toBe(6)
+        expect(day1.second("1221")).toBe(0)
+        expect(day1.second("123425")).toBe(4)
+        expect(day1.second("123123")).toBe(12)
+        expect(day1.second("12131415")).toBe(4)
+    }
 }
