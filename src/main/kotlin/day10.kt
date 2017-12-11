2bd6a2dc537f7ee44aa6fa23c2bf2804d034b4fc
Emilian Losneanu <mercer.traieste@gmail.com>
2017-12-11 07:37:44 +0200

    day 10 incomplete bead model

--- src/main/kotlin/day10.kt
+++ src/main/kotlin/day10.kt
@@ -1,7 +1,109 @@
-class Day10 {
+//
+// References:
+// https://dzone.com/articles/solving-the-josephus-problem-in-kotlin
 
-    fun run(circle: Int, seed: String): Pair<Int, Int> {
+class CircularHash {
+
+    fun calculate(circularListSize: Int, lengths: List<Int>): Pair<Int, Int> {
+        var circle = generateList(circularListSize)
+        println(circle); println()
+
+        for (length in lengths) {
+            circle = circle.pinchAndTwist(length);
+            println(circle); println()
+        }
+
         return Pair(-1, -1)
+    }
+
+    class CircleList(
+        private val items: List<Int>,
+        private val currentPosition: Int,
+        private val skipSize: Int
+    ) {
+
+        private val firstBead = Bead(items[0])
+
+        init {
+            var currentBead = firstBead
+            var nextBead = firstBead
+            for (item in items.subList(1, items.lastIndex + 1)) {
+                nextBead = Bead(item)
+                currentBead.next = nextBead
+                currentBead = nextBead
+            }
+            nextBead.next = firstBead
+
+            debugBeads()
+        }
+
+        private fun debugBeads() {
+            var currentBead = firstBead
+            var beadsToList = listOf<Int>()
+            for (i in 1..items.size) {
+                beadsToList = beadsToList.plus(currentBead.position)
+                currentBead = currentBead.next
+            }
+            println("beadsToList=$beadsToList")
+        }
+
+        fun pinchAndTwist(length: Int): CircleList {
+            println("length=$length")
+            val nextItems = pickBeadsToTwist(length)
+                .asReversed()
+                .plus(pickRemainingBeads(length))
+            val nextCurrentPosition = length + skipSize
+            return CircleList(nextItems, nextCurrentPosition, skipSize + 1)
+        }
+
+        private fun pickBeadsToTwist(length: Int): List<Int> {
+            var currentBead = firstBead
+            for (i in 1..currentPosition) {
+                currentBead = currentBead.next
+            }
+            var returnItems = listOf<Int>()
+            for (i in 1..length) {
+                returnItems = returnItems.plus(currentBead.position)
+                currentBead = currentBead.next
+            }
+            println("pickfirstitems=$returnItems")
+            return returnItems
+        }
+
+        private fun pickRemainingBeads(length: Int): List<Int> {
+            var currentBead = firstBead
+            for (i in 1..currentPosition) {
+                currentBead = currentBead.next
+            }
+            for (i in 1..length) {
+                currentBead = currentBead.next
+            }
+            var returnItems = listOf<Int>()
+            for (i in 1..items.size - length) {
+                returnItems = returnItems.plus(currentBead.position)
+                currentBead = currentBead.next
+            }
+            println("picklastitems=$returnItems")
+            return returnItems
+        }
+
+        override fun toString(): String {
+            return "CircleList(items=$items, currentPosition=$currentPosition, skipSize=$skipSize)"
+        }
+
+    }
+
+    class Bead(val position: Int) {
+        lateinit var next: Bead
+        lateinit var previous: Bead
+    }
+
+    private fun generateList(circularListSize: Int): CircleList {
+        var count = 0
+        val items = generateSequence {
+            (count++).takeIf { it < circularListSize }
+        }.toList()
+        return CircleList(items, 0, 0)
     }
 
 }
--- src/test/kotlin/day10.kt
+++ src/test/kotlin/day10.kt
@@ -2,16 +2,16 @@ import ch.tutteli.atrium.api.cc.en_UK.toBe
 import ch.tutteli.atrium.verbs.expect.expect
 import org.junit.Test
 
-class Day10Test {
+class CircularHashTest {
 
     @Test
-    fun randomInput() {
-        expect(Day10().run(6, "3, 4, 1, 5")).toBe(Pair(12, -1))
+    fun example() {
+        expect(CircularHash().calculate(5, listOf(3, 4, 1, 5))).toBe(Pair(12, -1))
     }
 
     @Test
     fun run() {
-        println("solution=" + Day10().run(256, "199,0,255,136,174,254,227,16,51,85,1,2,22,17,7,192"))
+        println("solution=" + CircularHash().calculate(256, listOf(199, 0, 255, 136, 174, 254, 227, 16, 51, 85, 1, 2, 22, 17, 7, 192)))
     }
 
 }
